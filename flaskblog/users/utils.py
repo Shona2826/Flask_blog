bf80c6874307e595ef7ff0fe6fbffa8564c81ce8
suman151 <sumangoel151@gmail.com>
2020-06-07 18:41:57 +0530

    Added files

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -2,7 +2,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for, current_app
+from flask import url_for, current_app,flash
 from flask_mail import Message
 from flaskblog import mail
 
@@ -22,12 +22,4 @@ def save_picture(form_picture):
 
 
 def send_reset_email(user):
-    token = user.get_reset_token()
-    msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
-                  recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
-{url_for('users.reset_token', token=token, _external=True)}
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
-    mail.send(msg)
+    pass
