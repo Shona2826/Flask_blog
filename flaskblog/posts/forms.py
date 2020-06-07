f1eb5aeae9f1f384004a4a34caaf48a57f7226a0
suman151 <sumangoel151@gmail.com>
2020-06-07 21:48:03 +0530

    Added files

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,URL
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    link = StringField('Link',validators=[URL()])
     submit = SubmitField('Post')
